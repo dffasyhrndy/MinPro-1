d8a70e3931507a899ed883896e3f8e938ae10fc1
Daffa Syahrandy Husain <drandy434@gmail.com>
2024-09-24 14:11:51 +0800

    Add files via upload

---
+++ praktikumdaspro.py
@@ -0,0 +1,25 @@
+for i in range (5):
+    i += 1
+    print(i)
+print("==================")
+for char in "Daffa":
+    if char == "D":
+        print(char)
+print("==================")
+buahan = ("Mangga", 'Apel', "Semangka")
+for buah in buahan:
+    if buah == "Mangga":
+        print(buah)
+print("==================")
+list =[1, 2, 3, 4, 5]
+for angka in list:
+    if angka == 1:
+         print(angka)
+print("===================")
+i=0
+while i < 10:
+	print(i)
+    
+	if i == 11:
+		break
+	i += 1
